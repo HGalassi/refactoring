09608a90304ac4026b3ac67684d0554d5896e75b
unknown <hgalassi17@gmail.com>
2021-07-07 23:46:16 -0300

    Refactoring Applying CHANGE FUNCTION DECLARATION - [PAG:48]
     Refactoring applying SLIDE STATEMENTS and EXTRACT FUNCTION
    
     Refactoring applying RENAMING PATTERN and INLINE VARIABLE
    
     Refactoring applying RENAMING PATTERN
    
     Refactoring applying SPLIT PHASE and EXTRACT FUNCTION
    
     Refactoring applying CHANGE FUNCTION DECLARATION
    
     Refactoring Applying CHANGE FUNCTION DECLARATION
    
     Refactoring applying CHANGE FUNCTION DECLARATION
    
     Refactoring applying IMMUTABLE OBJECT

--- refactoring/statement.js
+++ refactoring/statement.js
@@ -3,68 +3,84 @@ const plays = require('./plays');
 var res = statement(invoices,plays);
 
 function statement (invoice,plays){
-    let totalAmount = 0;
-    let result = `Statement for ${invoice.customer} \n`;
-    
-    for (let perf of invoice.performances){
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    return renderPlainText(statementData,plays);
 
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
-    }
-
-        result += `Amount owed is ${usd(totalAmount/100)}\n`;
-        result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
-        console.log(result);
+    function enrichPerformance(aPerformance){
+        const result = Object.assign({}, aPerformance);
         return result;
+    }
  }
 
-function amountFor(aPerformance){
-    let result = 0;
+ function renderPlainText(data,plays){
+    let result = `Statement for ${data.customer} \n`;
 
-    switch(playFor(aPerformance).type){
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30){
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy": 
-        result = 30000;
-            if (aPerformance.audience > 20){
-                result += 1000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error (`unknown type:${playFor(aPerformance).type}`);
+    for (let perf of data.performances){
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     }
-
+    result += `Amount owed is ${usd(totalAmount()/100)}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    console.log("Compiled");
     return result;
-}
 
-function playFor(aPerformance){
-    return plays[aPerformance.playID];
-}
-
-function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30,0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-}
-
-function usd(aNumber){
-    return new Intl.NumberFormat("en-US",
-                                { style: "currency", currency:"USD",
-                                    minimumFractionDigits: 2}).format(aNumber);
-}
-
-function totalVolumeCredits(invoice){
-    let volumeCredits = 0;
-    for (let perf of invoice.performances){
-        volumeCredits += volumeCreditsFor(perf);
+    function amountFor(aPerformance){
+        let result = 0;
+        switch(playFor(aPerformance).type){
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30){
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy": 
+            result = 30000;
+                if (aPerformance.audience > 20){
+                    result += 1000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error (`unknown type:${playFor(aPerformance).type}`);
+        }
+        return result;
     }
-    return volumeCredits;    
-}
+    
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
+    }
+    
+    function volumeCreditsFor(aPerformance){
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30,0);
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+    
+    function usd(aNumber){
+        return new Intl.NumberFormat("en-US",
+                                    { style: "currency", currency:"USD",
+                                        minimumFractionDigits: 2}).format(aNumber);
+    }
+    
+    function totalVolumeCredits(){
+        let result = 0;
+        for (let perf of data.performances){
+            result += volumeCreditsFor(perf);
+        }
+        return result;    
+    }
+    
+    function totalAmount(){
+        let result = 0;
+        for (let perf of data.performances){
+            result += amountFor(perf);
+        }
+        return result;
+    }
+    
+ }
+
 
  module.exports = statement;
